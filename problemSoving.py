2db566ed6be9ac4e942cb68f26bb16526e70f07f
saiki <saikiran.bushigolla@gmail.com>
2025-08-18 12:45:01 +0530

    problemSolving

---
+++ problemSoving.py
@@ -0,0 +1,53 @@
+# name = input("enter a charecter :")
+# for char in name:
+#     if name.isupper():
+#         print(name,"is upper")
+#     else:
+#         print(name,"is lower")   
+
+
+# word = input('enter a word: ')
+
+
+#Write a program to print middle character(s) in the given string or number. 
+# length = len(word)
+# mid = length//2
+# if length%2==0:
+#     print(word[mid-1]+word[mid])
+# else:
+#     print(word[mid])
+
+
+#Write a program to print the vowels in the given string in reverse order.  
+# str1 = input("type your word: ")
+# revStr1 = str1[::-1]
+# for ch in revStr1:
+#     if ch in ["a","e","i","o","u"]:
+#         print(ch,end="")
+      
+    
+
+
+#counting digits
+
+# n = int(input())
+# c = 0
+# while n>0:
+#     r = n%10
+#     c = c+1
+#     n = n//10
+# print(c) 
+
+#counting even digits
+# n= int(input())
+# c = 0
+# while n>0:
+#     r = n%10
+#     if r%2==0:
+#         c = c+1
+#     n = n//10
+# print(c) 
+
+
+
+
